17a9e27bc0be4e5c4f6c7336bfd35d42add6784b
elpaablo <donopwhile1@gmail.com>
2025-04-06 20:11:10 +0100

    Migrate LineageSettings to Settings

--- src/com/android/launcher3/util/SettingsCache.java
+++ src/com/android/launcher3/util/SettingsCache.java
@@ -34,8 +34,6 @@ import com.android.launcher3.dagger.ApplicationContext;
 import com.android.launcher3.dagger.LauncherAppSingleton;
 import com.android.launcher3.dagger.LauncherBaseAppComponent;
 
-import lineageos.providers.LineageSettings;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -80,9 +78,6 @@ public class SettingsCache extends ContentObserver implements SafeCloseable {
 
     private static final String SYSTEM_URI_PREFIX = Settings.System.CONTENT_URI.toString();
     private static final String GLOBAL_URI_PREFIX = Settings.Global.CONTENT_URI.toString();
-
-    private static final String LINEAGE_SYSTEM_URI_PREFIX =
-            LineageSettings.System.CONTENT_URI.toString();
 
     /**
      * Caches the last seen value for registered keys.
@@ -189,8 +184,6 @@ public class SettingsCache extends ContentObserver implements SafeCloseable {
             newVal = Settings.System.getInt(mResolver, key, defaultValue) == 1;
         } else if (keyUri.toString().startsWith(GLOBAL_URI_PREFIX)) {
             newVal = Settings.Global.getInt(mResolver, key, defaultValue) == 1;
-        } else if (keyUri.toString().startsWith(LINEAGE_SYSTEM_URI_PREFIX)) {
-            newVal = LineageSettings.System.getInt(mResolver, key, defaultValue) == 1;
         } else { // SETTING_SECURE
             newVal = Settings.Secure.getInt(mResolver, key, defaultValue) == 1;
         }
@@ -206,8 +199,6 @@ public class SettingsCache extends ContentObserver implements SafeCloseable {
             newVal = Settings.System.getInt(mResolver, key, defaultValue);
         } else if (keyUri.toString().startsWith(GLOBAL_URI_PREFIX)) {
             newVal = Settings.Global.getInt(mResolver, key, defaultValue);
-        } else if (keyUri.toString().startsWith(LINEAGE_SYSTEM_URI_PREFIX)) {
-            newVal = LineageSettings.System.getInt(mResolver, key, defaultValue);
         } else { // SETTING_SECURE
             newVal = Settings.Secure.getInt(mResolver, key, defaultValue);
         }

35381bdecec00bbc0fc9deebc25becda4d2cdec9
elpaablo <donopwhile1@gmail.com>
2025-08-27 22:27:16 +0100

    Use AOSP Settings

--- src/com/android/launcher3/util/SettingsCache.java
+++ src/com/android/launcher3/util/SettingsCache.java
@@ -34,8 +34,6 @@ import com.android.launcher3.dagger.LauncherBaseAppComponent;
 import com.android.launcher3.concurrent.annotations.LightweightBackground;
 import static com.android.launcher3.concurrent.annotations.LightweightBackgroundPriority.UI;
 
-import lineageos.providers.LineageSettings;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -84,7 +82,7 @@ public class SettingsCache extends ContentObserver {
     private static final String GLOBAL_URI_PREFIX = Settings.Global.CONTENT_URI.toString();
 
     private static final String LINEAGE_SYSTEM_URI_PREFIX =
-            LineageSettings.System.CONTENT_URI.toString();
+            Settings.System.CONTENT_URI.toString();
 
     private final Function<Uri, CopyOnWriteArrayList<OnChangeListener>> mListenerMapper = uri -> {
         registerUriAsync(uri);
@@ -192,7 +190,7 @@ public class SettingsCache extends ContentObserver {
         } else if (keyUri.toString().startsWith(GLOBAL_URI_PREFIX)) {
             newVal = Settings.Global.getInt(mResolver, key, defaultValue) == 1;
         } else if (keyUri.toString().startsWith(LINEAGE_SYSTEM_URI_PREFIX)) {
-            newVal = LineageSettings.System.getInt(mResolver, key, defaultValue) == 1;
+            newVal = Settings.System.getInt(mResolver, key, defaultValue) == 1;
         } else { // SETTING_SECURE
             newVal = Settings.Secure.getInt(mResolver, key, defaultValue) == 1;
         }
@@ -209,7 +207,7 @@ public class SettingsCache extends ContentObserver {
         } else if (keyUri.toString().startsWith(GLOBAL_URI_PREFIX)) {
             newVal = Settings.Global.getInt(mResolver, key, defaultValue);
         } else if (keyUri.toString().startsWith(LINEAGE_SYSTEM_URI_PREFIX)) {
-            newVal = LineageSettings.System.getInt(mResolver, key, defaultValue);
+            newVal = Settings.System.getInt(mResolver, key, defaultValue);
         } else { // SETTING_SECURE
             newVal = Settings.Secure.getInt(mResolver, key, defaultValue);
         }
